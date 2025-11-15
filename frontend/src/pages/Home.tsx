4357a1cbfa867ba6fdf6fb59bd058ebcbbcb6ff9
Andreea Daniela Burnaz <danielaburnaz@gmail.com>
2025-11-15 09:27:08 +0200

    Landing page done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,60 @@
+import TextType from "@/components/TextType";
+import SpotlightCard from "@/components/SpotlightCard";
+import PrismaticBurst from "@/components/PrismaticBurst";
+import GradientText from "@/components/GradientText";
+import { InputWithButton } from "@/components/InputWithButton";
+
 export const Home = () => {
   return (
-    <div className="container">
-      <h1 className="text-3xl font-bold">Welcome to the Web Application</h1>
-      <h1>Welcome to the Web Application</h1>
-      <p>This is the home page</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+        <PrismaticBurst
+          animationType="rotate3d"
+          intensity={2}
+          speed={0.5}
+          distort={1.0}
+          paused={false}
+          offset={{ x: 0, y: 0 }}
+          hoverDampness={0.25}
+          rayCount={24}
+          mixBlendMode="lighten"
+          colors={["#ff007a", "#4d3dff", "#ffffff"]}
+        />
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center gap-6">
+          <h1 className="text-4xl md:text-6xl font-bold mb-2 whitespace-nowrap">
+            Welcome to {""}
+            <span className="inline">
+              <GradientText
+                colors={["#ff007a", "#4d3dff", "#ffffff"]}
+                animationSpeed={3}
+                showBorder={false}
+                className="custom-class inline"
+              >
+                {"Sephira"}
+              </GradientText>
+            </span>
+          </h1>
+          <TextType
+            className="text-1xl md:text-4xl mb-2 font-bold color-gray-300"
+            text={[
+              "Your personalized marketing tool",
+              "Automate painfull tasks",
+              "One manager, a thousand campaigns",
+            ]}
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="_"
+          />
+          <SpotlightCard
+            className="flex flex-col items-center justify-center custom-spotlight-card"
+            spotlightColor="rgba(0, 229, 255, 0.2)"
+          >
+            <p className="mb-4">Enter your Braze Canvas ID to get started</p>
+            <InputWithButton />
+          </SpotlightCard>
+        </div>
+      </div>
     </div>
   );
 };
